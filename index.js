35f00dd28440cf7106f792964da9c2a838d1467b
heymexa <heymexa@gmail.com>
2019-10-31 22:21:30 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
+const uuidv4 = require('uuid/v4');
 const { makeRandomExampleRow } = require('./src/utils');
 
 const outputFile = './output.pdf';
 const fontFile = './src/font.ttf';
 
+function getOutputFile() {
+  return `${uuidv4()}.pdf`;
+}
+
 const doc = new PDFDocument();
-doc.pipe(fs.createWriteStream(outputFile));
+doc.pipe(fs.createWriteStream(getOutputFile()));
 doc.font(fontFile);
 
 doc
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,32 @@
 const random = require('lodash/random');
 
-function makeRandomExampleRow(doc, margin = 5.2, width = 50, step = 130, column = 3) {
+function makeRandomExampleRow(
+  doc,
+  margin = 5.2,
+  width = 50,
+  step = 130,
+  column = 3
+) {
   for (let i = 0; i <= column; i++) {
-    makeRandomExample(doc, { width: width + step * i });
+    level1(doc, { width: width + step * i });
     if (i !== column) {
       doc.moveUp(margin);
     }
   }
 }
 
-function makeRandomExample(doc, options = {}) {
-  makeColumnExample(doc, random(10, 99), random(10, 99), '+', options);
+function rnd(from, to) {
+  return Math.floor(Math.random() * (to - from)) + from;
+}
+
+function level1(doc, options = {}) {
+  makeColumnExample(doc, rnd(10, 999), rnd(10, 999), '+', options);
+  doc.moveDown(3);
+}
+
+function level2(doc, options = {}) {
+  const numbers = new Array(2).fill(1).map(() => rnd(10, 99)).sort((a, b) => b - a);
+  makeColumnExample(doc, numbers[0], numbers[1], '-', options);
   doc.moveDown(3);
 }
 
@@ -20,8 +36,11 @@ function makeColumnExample(doc, number1, number2, sign, options = {}) {
   doc.text(number1, { width, align });
   doc.moveDown(0.1);
   doc.moveUp(0.5);
-  const signMargin = (Math.max(String(number1).length, String(number2).length) + 1) * 10;
-  doc.text(`${sign}`, { align, width: width - signMargin });
+  let signMargin =
+    (Math.max(String(number1).length, String(number2).length) + 1) * 10;
+  const signWidth = width - signMargin <= 12 ? 12 : width - signMargin;
+  console.log(`width - signMargin: ${width - signMargin}`);
+  doc.text(`${sign}`, { align, width: signWidth });
   doc.moveUp(0.5);
   doc.text(number2, { width, align });
   doc.moveUp(0.9);
